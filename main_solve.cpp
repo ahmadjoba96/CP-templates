c2255b488aa663bf0ec36dc472353422b94dc896
Ahmad Joba <ahmadjoba2005@gmail.com>
2025-10-24 12:02:39 +0300

    add solve template

---
+++ main_solve.cpp
@@ -0,0 +1,59 @@
+// ✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧
+//
+//                   ⫷⫷⫷  ★ Ahmad Joba ★  ⫸⫸⫸
+//              ⫷⫷  ✦ Wrong answer on test 256! ✦  ⫸⫸
+//
+// ✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧✧
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define ll long long
+#define mod 1000000007LL
+#define debug(x) cout<<"[" << #x <<" : "<< x <<"]"<<endl;
+#define f(i,s,e) for(int i=s;i<e;i++)
+#define rf(i,e,s) for(int i=e-1;i>=s;i--)
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define not_less_than(v, n) lower_bound(v.begin(), v.end(), n) // >= n
+#define not_more_than(v, n) lower_bound(v.rbegin(), v.rend(), n, greater<int>()) // <= n
+#define more_than(v, n) upper_bound(v.begin(), v.end(), n) // > n
+#define less_than(v, n) upper_bound(v.rbegin(), v.rend(), n, greater<int>()) // < n
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define print(a) for(auto x : a) cout << x << " "; cout << endl
+#define print1(a) for(auto x : a) cout << x.F << " " << x.S << endl
+#define op() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define test int tc; cin>>tc; while(tc--)
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+long long digits(int n) {return log(n) / log(10) + 1;}
+
+void solve()
+{
+
+
+
+
+}
+
+int32_t main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0), cout.tie(0);
+#ifndef ONLINE_JUDGE
+  freopen("", "r", stdin);
+  freopen("", "w", stdout);
+#endif
+
+  op();
+  //test
+  {
+    solve();
+  }
+  cerr << "A7mad_Joba" << endl;
+  return 0;
+}
